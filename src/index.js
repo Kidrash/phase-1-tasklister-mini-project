0840b51f00e50cc063efa57a24c4a58aca825c07
Kidrash <markkaranja11@gmail.com>
2022-08-29 10:38:58 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form =  document.querySelector('form')
+  form.addEventListener('submit', (e) =>{
+  e.preventDefault()
+  handleTask(e.target.new_task_description.value)
+  form.reset()
+})
+function handleTask(newTask){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+
+  btn.textContent = "x"
+  btn.addEventListener("click", handleDelete)
+
+  p.textContent = newTask
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p);
+}
+  function handleDelete(e){
+  e.target.parentNode.remove()
+
+}  // your code here
 });
